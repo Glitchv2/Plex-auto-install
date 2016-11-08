d55aea543fa27225c4ffff2ccde47eacf9ac1cdd
Glitchv2 <Glitchv2@users.noreply.github.com>
2016-11-07 20:03:06 -0600

    Update download&install.sh

--- download&install.sh
+++ download&install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 clear
+#Greps the host OS name
+cat /etc/*-release | grep -ie "centos" -ie "ubuntu" -ie "fedora" | awk '{print $1}'
+
+shopt -s nocasematch
+
+OSNAME='cat /etc/*-release | awk '{print $1}'
+
 
 #Checks if user is root got install.
 if [[ $EUID -ne 0 ]]; then
@@ -7,7 +14,30 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-mkdir -p /tmp/plexmediaserver
-wget -p /tmp/plexmediaserver/ https://downloads.plex.tv/plex-media-server/1.2.6.2975-9394c87/plexmediaserver_1.2.6.2975-9394c87_amd64.deb
+cat /etc/*-release | grep -ie "centos" -ie "ubuntu" -ie "fedora" | awk '{print $1}'
 
-dpkg -i /tmp/plexmediaserver/
+if [ ['echo $1 | tr [:lower:]' = 'echo "centos" | tr [:lower]' ]; then
+
+#if OS is Centos then install proper RPM
+OSNAME="CentOS"
+yum install https://downloads.plex.tv/plex-media-server/1.2.6.2975-9394c87/plexmediaserver-1.2.6.2975-9394c87.x86_64.rpm -y
+continue
+
+elif [ ['echo $1 | tr [:lower:]' = 'echo "ubuntu" | tr [:lower]' ]; then
+
+OSNAME="Ubuntu"
+dpkg -i https://downloads.plex.tv/plex-media-server/1.2.6.2975-9394c87/plexmediaserver_1.2.6.2975-9394c87_amd64.deb
+continue
+
+elif [ ['echo $1 | tr [:lower:]' = 'echo "fedora" | tr [:lower]' ]; then
+
+OSNAME="Fedora"
+yum install https://downloads.plex.tv/plex-media-server/1.2.6.2975-9394c87/plexmediaserver-1.2.6.2975-9394c87.x86_64.rpm
+continue
+
+elif [ ['echo $1 | tr [:lower:]' = 'echo "Unknown" | tr [:lower]' ]; then
+echo "OS UNKNOWN! Ending my life!" 2>&1
+fi
+
+
+shopt -u nocasematch
